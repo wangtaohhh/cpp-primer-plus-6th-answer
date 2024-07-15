270440b4a5673664cf6cf001a9393f66fcf32edc
wangtaohhh <wangtaolab@tom.com>
2024-07-15 16:13:55 +0800

    20240715

--- unit7/7_3.cpp
+++ unit7/7_3.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
+struct box {
+    char maker[40];
+    float height;
+    float width;
+    float length;
+    float volume;
+};
+
+void output(box bx){
+    std::cout << bx.maker << std::endl;    
+    std::cout << bx.height << std::endl;    
+
+    std::cout << bx.width << std::endl;    
+    std::cout << bx.length << std::endl;    
+    std::cout << bx.volume << std::endl;    
 
 
-int main(){
-    std::cout << "Enter the score, -1 to quit: " << std::endl;    
+}
+
+void calcu(box *bx) {
+    bx -> volume = bx -> height * bx -> length * bx -> width;
+
+
+
+
+
+
+
+}
+
+int main() {
+    box bx = {"Jay", 0.49, 2.94, 0.49, 0.0};
+
+    output(bx);
+    calcu(&bx);
+
+    std::cout << "\n--\n";
+    output(bx);
+
+    return 0;
 }
